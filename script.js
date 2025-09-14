d74797b7b30f9ff394cc3b7ba879019d9a1aa621
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-14 13:30:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const startBtn = document.getElementById("start-btn");
 const canvas = document.getElementById("canvas");
-
 const startScreen = document.querySelector(".start-screen");
 const checkpointScreen = document.querySelector(".checkpoint-screen");
+
+const checkpointMessage = document.querySelector(".checkpoint-screen > p");

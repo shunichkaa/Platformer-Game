5fa84375c4e09f31fa73af557250b42a4b0c9557
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-15 15:28:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,6 @@ const checkpointMessage = document.querySelector(".checkpoint-screen > p");
 const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-
 const gravity = 0.5;
+
+let isCheckpointCollisionDetectionActive = true;

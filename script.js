bbc12b4ac3b32678520ddf95cc47b9e50e616abc
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-17 12:33:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -34,8 +34,12 @@ class Player {
 
 update() {
   this.draw();
-  this.position.x += this.velocity.x; 
+  this.position.x += this.velocity.x;
   this.position.y += this.velocity.y;
+
+  if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+    
+  }
 }
 
 

d3805ccd2283a3f61943791759ceb14a1f3f89ec
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-20 14:20:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -82,16 +82,13 @@ const keys = {
     pressed: false
   }
 };
-
-
 const movePlayer = (key, xVelocity, isPressed) => {
   if (!isCheckpointCollisionDetectionActive) {
     player.velocity.x = 0
     player.velocity.y = 0
+    return
   }
 }
-
-
 
 const startGame = () => {
   canvas.style.display = "block";

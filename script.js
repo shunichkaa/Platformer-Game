78f0bc03abb37fbdc71df3c7f235bf5aa868a5c8
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-21 12:48:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -56,20 +56,13 @@ class Player {
   }
 }
 
-class Platform {
-  constructor(x, y, width, height, color = "brown") {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.color = color;
-  }
 
-  draw(ctx) {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+class Platform {
+  constructor(x, y) {
+
   }
 }
+
 
 const player = new Player();
 

b10a37fa3dff03cee4d97c17f83f10da6e283a99
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-24 15:43:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -71,14 +71,18 @@ class Platform {
   }
 }
 
+
 class CheckPoint {
   constructor(x, y, z) {
-    this.position = { x, y };
+    this.position = {
+      x,
+      y,
+    };
     this.width = proportionalSize(40);
     this.height = proportionalSize(70);
-  }
-}
-
+  this.claimed = false;
+  };
+};
 
 
 const player = new Player();

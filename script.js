c72d939fafb439755e1fb92eacd2c628f6d6d177
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-25 14:46:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -196,8 +196,12 @@ const animate = () => {
 
 checkpoints.forEach((checkpoint, index, checkpoints) => {
   const checkpointDetectionRules = [
-    player.position.x >= checkpoint.position.x
+    player.position.x >= checkpoint.position.x,
+    player.position.y >= checkpoint.position.y,
+    player.position.y + player.height <= checkpoint.position.y + checkpoint.height,
+    isCheckpointCollisionDetectionActive
   ];
+
 });
 }
 

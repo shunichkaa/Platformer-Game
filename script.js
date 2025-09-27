46461e5eaf18a0e1bc6aae2a5c00816f48d4d1a7
Aleksandra Mate <matealeksandra@gmail.com>
2025-09-27 11:28:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -193,22 +193,26 @@ const animate = () => {
       player.velocity.y = gravity;
     };
   });
-checkpoints.forEach((checkpoint, index, checkpoints) => {
-  const checkpointDetectionRules = [
-    player.position.x >= checkpoint.position.x,
-    player.position.y >= checkpoint.position.y,
-    player.position.y + player.height <=
-      checkpoint.position.y + checkpoint.height,
-    isCheckpointCollisionDetectionActive,
-    player.position.x - player.width <=
-      checkpoint.position.x - checkpoint.width + player.width * 0.9,
-    index === 0 || checkpoints[index - 1].claimed === true,
-  ];
+
+  checkpoints.forEach((checkpoint, index, checkpoints) => {
+    const checkpointDetectionRules = [
+      player.position.x >= checkpoint.position.x,
+      player.position.y >= checkpoint.position.y,
+      player.position.y + player.height <=
+        checkpoint.position.y + checkpoint.height,
+      isCheckpointCollisionDetectionActive,
+      player.position.x - player.width <=
+        checkpoint.position.x - checkpoint.width + player.width * 0.9,
+      index === 0 || checkpoints[index - 1].claimed === true,
+    ];
 
   if (checkpointDetectionRules.every((rule) => rule)) {
+    checkpoint.claim();
   }
-});
+
+  });
 }
+
 
 const keys = {
   rightKey: {
